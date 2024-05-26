07e824ccad32b7ad764a0927f0f3e34114cf7502
sewmini.rathnaweera <126885819+TharushiSewmini@users.noreply.github.com>
2024-05-26 20:13:00 +0530

    adding hover issues

--- src/components/DrawerComponent/index.tsx
+++ src/components/DrawerComponent/index.tsx
@@ -1,42 +1,49 @@
-
 import "./index.css";
 import arrowleft from "../../assets/left.png";
+
 interface DrawerComponentProps {
   isExpand: boolean;
   onClick: (e: any) => void;
 }
+
 const DrawerComponent = ({ isExpand, onClick }: DrawerComponentProps) => {
   return (
     <div
       className="drawer-main-container"
-      style={{
-        display: isExpand ? "flex" : "none",
-      }}
+      style={{ display: isExpand ? "flex" : "none" }}
     >
       <div className="home-page-left-small-container-bg">
         <div className="home-page-left-small-container">
           <div className="home-page-left-small-c-left">
             {/* <div className="home-page-left-small-container-logo">Sew</div> */}
             <div className="home-page-left-small-container-tabs">
-              <li className="home-page-left-small-container-tab">Home</li>
-
-              <li className="home-page-left-small-container-tab">Projects</li>
-
-              <li className="home-page-left-small-container-tab">Experience</li>
-
-              <li className="home-page-left-small-container-tab">Contact Me</li>
+              <a
+                href="#"
+                className="home-page-left-small-container-tab"
+                style={{
+                    cursor:"pointer"
+                }}
+              >
+                Home
+              </a>
+              <a href="#" className="home-page-left-small-container-tab">
+                Projects
+              </a>
+              <a href="#" className="home-page-left-small-container-tab">
+                Experience
+              </a>
+              <a href="#" className="home-page-left-small-container-tab">
+                Contact Me
+              </a>
             </div>
-           
           </div>
           <div className="home-page-left-small-c-right">
             <img
               src={arrowleft}
               className="home-page-left-small-arrow-right"
               onClick={onClick}
-              style={{
-                display: "flex",
-              }}
-            />{" "}
+              alt="Arrow Left"
+            />
           </div>
         </div>
       </div>
