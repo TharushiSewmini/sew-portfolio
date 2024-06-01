d32517b6038d39d9a6ec7c6062532b5033ac3db7
sewmini.rathnaweera <126885819+TharushiSewmini@users.noreply.github.com>
2024-06-01 17:14:06 +0530

    new chnages

--- src/components/ReusableButtonComponenet/index.tsx
+++ src/components/ReusableButtonComponenet/index.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import "./index.css";
-import pdf from "../../assets/sewmini_2024.pdf"
+import pdf from "../../assets/tharushi_sewmini.pdf";
 const ReusableButton = () => {
   return (
     <div className="reusablebutton-container">
       <button className="reusable-button-c">
-        <a href={pdf} download='sewmini_2024.pdf' className="reusable-btn-cv-download"> Download CV</a>
+        <a
+          href={pdf}
+          download="tharushi sewmini Curriculum vitae"
+          className="reusable-btn-cv-download"
+        >
+          {" "}
+          Download CV
+        </a>
       </button>
     </div>
   );
